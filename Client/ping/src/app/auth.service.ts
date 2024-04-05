61bc12a64de7258d488db1d904d9a3155f92e4a7
shobith.chandran <shobith.chandran@innovaturelabs.com>
2024-04-05 19:23:28 +0530

    Add Tailwind CSS and update dependencies

---
+++ Client/ping/src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    public afAuth: AngularFireAuth // Inject Firebase auth service
+  ) {}
+  // Sign in with Google
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log('You have been successfully logged in!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
